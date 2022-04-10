35a0e575b0545def48bc3878f78cfc90b65ed8bc
mrChocolatos <taov_rustam@mail.ru>
2022-04-10 21:01:48 +0300

    Create PO Finally commit

--- test_product_page.py
+++ test_product_page.py
@@ -76,7 +76,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.check_basket_is_empty_negative()
 
 
-@pytest.mark.need_review
+
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, browser):
@@ -94,6 +94,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
         product_page = ProductPage(browser, link)
